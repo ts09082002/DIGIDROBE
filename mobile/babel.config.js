2732ee402d0efac287de99c3923c516214e552bc
supreetverma1@gmail.com <supreetverma1@gmail.com>
2026-06-14 13:15:19 +0530

    fix drawable and release config

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,10 +1,14 @@
 module.exports = function (api) {
     api.cache(true);
+  
     return {
-        presets: ['babel-preset-expo'],
-        plugins: [
-            ['@babel/plugin-proposal-decorators', { legacy: true }],
-            'react-native-reanimated/plugin', // Must be listed last
-        ],
+      presets: ['babel-preset-expo'],
+  
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
+  
+        'react-native-reanimated/plugin', // MUST be last
+      ],
     };
-};
+  };
